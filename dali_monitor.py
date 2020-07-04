6a3c440edb86efb94623268644b6e1a9279fd597
shareef <shareef@penguin>
2020-07-04 15:37:01 +0100

    Adding dali_monitor

---
+++ dali_monitor.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import pigpio
+import _dali
+import atexit
+
+HOST='rpi-bc.jalloq.co.uk'
+LEDS = [17,13,12,16]
+RX = 23
+
+
+def init_leds():
+    for led in LEDS:
+        pi.write(led,1)      
+    for led in LEDS:
+        value = pi.read(led)
+        print('led %s got value %s' % (led,value))
+
+def cleanup():
+    rx.cancel()  
+    pi.stop()
+    print('cleaned up')
+
+def callback(frame):
+    """
+    Receives a Dali frame
+    """
+    print("Dali Monitor: %s" % hex(frame))
+
+pi = pigpio.pi(HOST)
+rx = _dali.rx(pi, RX, callback)
+
+atexit.register(cleanup)
+
+init_leds()
+
+while(True):
+    pass
